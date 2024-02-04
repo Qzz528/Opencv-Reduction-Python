7ba4d1d9f02293001c17c6b25cff01d23ab89ce0
Qzz528 <68725224+Qzz528@users.noreply.github.com>
2024-02-04 09:48:43 +0800

    fix

--- minmaxloc.py
+++ minmaxloc.py
@@ -16,15 +16,18 @@ import cv2
 import numpy as np
 
 #借助numpy复现minMaxLoc功能
-def minmaxloc_numpy(src):
+def minMaxLoc_numpy(src):
     min_val = np.min(src)
     max_val = np.max(src)
-    min_loc = np.argwhere(src==min_val)[0] #只需要返回一个位置
+    min_loc = np.argwhere(src==min_val)[0] #多个极值只需要返回一个位置
     max_loc = np.argwhere(src==max_val)[0]
-    return min_val, max_val, tuple(min_loc), tuple(max_loc)
+    #opencv返回的是先列再行
+    min_loc = (min_loc[1],min_loc[0])
+    max_loc = (max_loc[1],max_loc[0])
+    return min_val, max_val, min_loc, max_loc
 
 #不依赖其他库，复现minMaxLoc功能
-def minmaxloc(src):
+def minMaxLoc(src):
     assert len(src.shape)==2
     h,w = src.shape
     
@@ -36,10 +39,10 @@ def minmaxloc(src):
         for j in range(w):
             if src[i,j] > max_val:
                 max_val = src[i,j]
-                max_loc = (i,j)
+                max_loc = (j,i) #opencv返回的位置是先列再行
             if src[i,j] < min_val:
                 min_val = src[i,j]
-                min_loc = (i,j)
+                min_loc = (j,i)
                 
     return min_val, max_val, min_loc, max_loc
 
@@ -51,14 +54,19 @@ if __name__ == '__main__':
     src = np.arange(16).reshape(4,4).astype(float)
     print(src)
     print(cv2.minMaxLoc(src))
-    print(minmaxloc_numpy(src)) 
-    print(minmaxloc(src))
+    print(minMaxLoc_numpy(src)) 
+    print(minMaxLoc(src))
     
     print('\n')
 
     src = -256.7*np.ones(16).reshape(4,4)
     print(src)
     print(cv2.minMaxLoc(src))
-    print(minmaxloc_numpy(src))  
-    print(minmaxloc(src))
-  
+    print(minMaxLoc_numpy(src))  
+    print(minMaxLoc(src))
+
+    src = cv2.imread("./pics/LenaRGB.bmp")
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    print(cv2.minMaxLoc(gray))
+    print(minMaxLoc_numpy(gray))  
+    print(minMaxLoc(gray))
