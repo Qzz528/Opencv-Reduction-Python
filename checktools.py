974902d3e165f70304c8c608ae0665243691bfc6
Qzz528 <68725224+Qzz528@users.noreply.github.com>
2024-02-14 22:21:41 +0800

    Update checktools.py

--- checktools.py
+++ checktools.py
@@ -11,6 +11,13 @@ def MaxError(dst1, dst2):
     return err.max()
 
 #遍历params参数组合，求opencv方法func_cv和自写方法func的MaxError
-#def ParamsCheck(func_cv, func, params_dict):
-#    params_list = list(params_dict.keys())
-#    param_dict = 
+def ParamsCheck(func_cv, func, params_dict):
+    params_list = list(params_dict.keys())
+    n_params = [len(x) for x in params_dict.values()]
+    n_total = np.prod(n_params)
+    n_index = np.arange(n_total).reshape(*n_params)
+    for i in range(n_total):
+        params_index = np.argwhere(n_index==i)
+        params_temp = {}
+        for j,k in zip(params_list,params_index):
+            params_temp[j] = params_dict[j][k]
